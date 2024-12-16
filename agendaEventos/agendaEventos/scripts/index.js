81f90f61216eb1d2edac79fefbbdd3a84451cf89
Pedro Watermann <pedro.programmer.pt@gmail.com>
2024-12-15 22:41:25 -0300

    Botão para adicionar evento adicionado na página de início

--- agendaEventos/agendaEventos/scripts/index.js
+++ agendaEventos/agendaEventos/scripts/index.js
@@ -1,70 +1,76 @@
+// Obtém os dados do localStorage
+const eventos = JSON.parse(localStorage.getItem("eventos"));
+
+if (eventos) {
+    eventos.sort((a, b) => new Date(a.data) - new Date(b.data));
+
+    eventos.forEach(evento => {
+        const li = document.createElement('li');
+        const a = document.createElement("a");
+        const div = document.createElement("div");
+        const del = document.createElement("button");
+        const edit = document.createElement("button");
+
+        let data = formatData(evento.data);
+
+        a.className = "centered";
+        a.href = `editEventos.html?id=${evento.id}`;
+        a.innerHTML = `<strong><em>${evento.titulo}</em></strong><br><br>${data} <br> ${evento.hora}`;
+
+        div.className = "aligned";
+
+        del.textContent = "Excluir";
+        del.className = "btn";
+        del.classList.add("red");
+        del.onclick = () => {
+            window.location.href = `delEventos.html?id=${evento.id}`;
+        };
+
+        edit.textContent = "Editar";
+        edit.className = "btn";
+        edit.classList.add("orange");
+        edit.onclick = () => {
+            window.location.href = `editEventos.html?id=${evento.id}`;
+        };
+
+        div.appendChild(del);
+        div.appendChild(edit);
+
+        li.appendChild(a);
+        li.appendChild(del);
+        li.appendChild(edit);
+        li.id = "conteudo";
+
+        eventList.appendChild(li);
+    });
+}
+
+const eventList = document.getElementById('event-list');
+
 window.onload = () => {
     const linkDaPagina = location.href;
     if (linkDaPagina.includes("index")) {
         const index = document.querySelector("#index");
         index.classList.add("page");
     }
+
+    const add = document.createElement("button");
+    add.textContent = "Adicionar evento";
+    add.className = "btn";
+    add.classList.add("add");
+    add.onclick = () => {
+        window.location.href = "addEventos.html";
+    };
+    const elemento = document.querySelector("#conteudo");
+    if (!eventList.contains(elemento)) {
+        add.disabled = false;
+        eventList.appendChild(add);
+    } else {
+        eventList.removeChild(add);
+    }
 };
 
-// Obtém os dados do localStorage
-const eventos = JSON.parse(localStorage.getItem("eventos"));
-
-eventos.sort((a, b) => new Date(a.data) - new Date(b.data));
-
-const eventList = document.getElementById('event-list');
-
-eventos.forEach(evento => {
-    const li = document.createElement('li');
-    const a = document.createElement("a");
-    const div = document.createElement("div");
-    const del = document.createElement("button");
-    const edit = document.createElement("button");
-
-    let data = formatData(evento.data);
-
-    a.className = "centered";
-    a.href = `editEventos.html?id=${evento.id}`;
-    a.innerHTML = `<strong><em>${evento.titulo}</em></strong><br><br>${data} <br> ${evento.hora}`;
-
-    div.className = "aligned";
-
-    del.textContent = "Excluir";
-    del.className = "btn";
-    del.classList.add("red");
-    del.onclick = () => {
-        window.location.href = `delEventos.html?id=${evento.id}`;
-    };
-
-    edit.textContent = "Editar";
-    edit.className = "btn";
-    edit.classList.add("orange");
-    edit.onclick = () => {
-        window.location.href = `editEventos.html?id=${evento.id}`;
-    };
-
-    div.appendChild(del);
-    div.appendChild(edit);
-
-    li.appendChild(a);
-    // li.appendChild(div);
-    li.appendChild(del);
-    li.appendChild(edit);
-    
-    eventList.appendChild(li);
-});
-
-function formatData( dt ) {
-    /*const data = new Date(dt); // Exemplo de data
-
-    // Extrair o dia, mês e ano
-    const dia = String(data.getDate()).padStart(2, '0'); // Adiciona o zero à esquerda, se necessário
-    const mes = String(data.getMonth() + 1).padStart(2, '0'); // getMonth() retorna 0 para janeiro, então somamos 1
-    const ano = data.getFullYear(); // Pega o ano completo
-    
-    const dataFormatada = `${dia}/${mes}/${ano}`;
-    
-    return dataFormatada;*/
-
+function formatData(dt) {
     const data = dt.split("-");
     const dia = data[2];
     const mes = data[1];
